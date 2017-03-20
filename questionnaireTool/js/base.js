4b21b5f4361d7d33b00db2b1f5b260d090214be3
mafang <ma13240973572@sina.com>
2017-03-20 17:51:08 +0800

    拖拽

--- questionnaireTool/js/base.js
+++ questionnaireTool/js/base.js
@@ -1,10 +1,31 @@
-var widget = {};
-widget={
-   init:function(){
-	   widget.componentsInit();
-   },
-   componentsInit:function(){
-	   $("#end_summernote").summernote();
-   }
+//var widget = {};
+//widget={
+//   init:function(){
+//	   widget.componentsInit();
+//   },
+//   componentsInit:function(){
+//	   $("#end_summernote").summernote();
+//   }
+//}
+//widget.init();
+'use strict';
+var words = document.querySelector('[data-question]');
+//var sentence = document.querySelector('[data-sentence]');
+
+function resetPlaceholder() {
+    var s = document.querySelectorAll('.question');
+    if (!s.length) {
+        placeholder.style.display = 'block';
+    } else {
+        placeholder.style.display = 'none';
+    }
 }
-widget.init();
+document.addEventListener('DOMContentLoaded', function () {
+    var drag = dragula([
+        words/*,
+        sentence*/
+    ]);
+    //btn.addEventListener('click', submit);
+//    drag.on('drop', resetPlaceholder);
+//    drag.on('cancel', resetPlaceholder);
+});
