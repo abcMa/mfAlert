e51569fc6ba2078d21f83e7b87a27d0437f98644
mafang <ma13240973572@sina.com>
2017-08-08 11:34:04 +0800

    基础方法

--- copyJqueryDemo/js/copy-jquery-0.1.js
+++ copyJqueryDemo/js/copy-jquery-0.1.js
@@ -14,14 +14,53 @@ var $ = function(dom){
      * #dom-id选择器
      * dom-标签选择器
      * dom[attr=val]-属性选择器
-     *
      * 等选择器
      */
-     var curObj = $.select(dom);
-}
-$.prototype.css = function(){
+    var curObj = $.selector(dom);
 
+    var $this = {
+        css : function(ops,val){
+            if(!ops){
+                return false;
+            }
+            //以对象的方式传参
+            if(ops instanceof Object){
+                for(var index in ops){
+                    curObj.style[index] = ops[index];
+                }
+            }else{//以key,value的方式传参
+                curObj.style[ops] = val;
+            }
+        },
+        hide:function(){
+            curObj.style.display = "none";
+        },
+        show:function(type){
+            if(type){
+                if(type == "inline"){
+                    curObj.style.display = "inline";
+                }else if(type == "inlineBlock"){
+                    curObj.style.display = "inline-block";
+                }
+            }else{
+                curObj.style.display = "block";
+            }
+
+        },
+        on:function(type,target,fn){
+
+        }
+    }
+    return $this;
 }
-$.selector = function(d){
+
+
+//暂且只支持id选择器
+$.selector = function(id){
+    //判断是否是字符串
+    if(typeof id != "string"){
+      return false;
+    }
+    return document.getElementById(id.split("#")[1]);
 
 }
