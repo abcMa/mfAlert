38c4508b48930606862f770153b89133d2473846
mafang <ma13240973572@sina.com>
2017-08-15 16:30:37 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 /**
  * Created by sks on 2017/8/15.
  */
-var gulp = require('gulp');
-gulp.src('js/testGulp.js')         // 获取流的api
-    .pipe(gulp.dest('dist/foo.js')); // 写放文件的api
+var gulp = require('gulp'),
+rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');//js压缩插件
+
+gulp.task('default', function() {
+    gulp.src('js/testGulp.js')// 获取流的入口，也就是源文件
+        .pipe(uglify())//压缩
+        .pipe(rename('testGulp.min.js'))
+        .pipe(gulp.dest('build/js')); // 写放文件的api
+});
