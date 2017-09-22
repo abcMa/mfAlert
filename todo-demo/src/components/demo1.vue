c6dd40f404636713026b40d23c220299ae5702c2
mafang <ma13240973572@sina.com>
2017-09-22 14:01:06 +0800

    practice vue demo

---
+++ todo-demo/src/components/demo1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="demo">
+     <input v-model="dmo"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      demo:"hhaha"
+    }
+  }
+}
+</script>
+<style>
+
+</style>
